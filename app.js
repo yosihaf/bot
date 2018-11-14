6471d04cad4870b522dee6e8520652dc2a9bbe7f
yoseff2525 <yoseff2525@gmail.com>
2018-11-14 20:30:41 +0200

    init

--- app.js
+++ app.js
@@ -4,31 +4,94 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+let Category = {MANAGER:"Manager", WORKER:"Worker", CUSTOMER:"Customer", SUPPLIER:"Supplier"};
+
+
+let branches = [ {   id:  123 ,name:"jerusalem Flowers",  active: true , city:'jerusalem' },
+    {  id:  456 ,name:"raanana Flowers",  active: false, city:'raanana'}];
+
+
+let users = [
+    {
+        id:302951108,
+        name:'Yosef',
+        category:Category.MANAGER,
+        branchId:123,
+        userName:'abc',
+        password:'Aa123456',
+        valid:true
+    },
+    {
+        id:333222111,
+        name:'Shay',
+        category:Category.CUSTOMER ,
+        userName:'Shay' ,
+        password:'abc',
+        valid:false
+    },
+    {
+        id:123456789,
+        name:'Moshe',
+        category:Category.WORKER ,
+        userName:'Moshe' ,
+        password:'Aa123456',
+        valid:true
+    },
+    {
+        id:987654321,
+        name:'levi',
+        category:Category.CUSTOMER ,
+        userName:'levi' ,
+        password:'Aa123456',
+        valid:true
+    }
+];
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(logger('dev'));
+//app.use(express);
+//app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+
+
+app.get('/', function(req, res) {
+    res.render('home');
 });
 
+
+app.post('/about', function(req, res){
+    res.render('about');
+});
+
+app.listen(8000);
+console.log('8000 is the magic port');
+
+
+
+//module.exports = app;
+
+
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+/*app.use(function(req, res, next) {
+  next(createError(404));
+});*/
+
 // error handler
-app.use(function(err, req, res, next) {
+/*app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -36,6 +99,4 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-module.exports = app;
+});*/
